40f6a43cf855f5ef95f13d2a9daca58698883ade
DisRachik <radchenkoig@gmail.com>
2023-07-26 18:01:39 +0300

    feat:form submit outputs data to the console

--- App.js
+++ App.js
@@ -6,27 +6,27 @@ const image = require('./assets/photo-bg.png');
 import { RegistrationScreen, LoginScreen } from './src/screens';
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		Roboto: require('./assets/fonts/Roboto-Medium.ttf'),
-	});
+  const [fontsLoaded] = useFonts({
+    Roboto: require('./assets/fonts/Roboto-Medium.ttf'),
+  });
 
-	if (!fontsLoaded) {
-		return null;
-	}
+  if (!fontsLoaded) {
+    return null;
+  }
 
-	return (
-		<ImageBackground source={image} resizeMode='cover' style={styles.imageBg}>
-			{/* <RegistrationScreen /> */}
-			<LoginScreen />
-			<StatusBar style='auto' />
-		</ImageBackground>
-	);
+  return (
+    <ImageBackground source={image} resizeMode="cover" style={styles.imageBg}>
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
+      <StatusBar style="auto" />
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-	imageBg: {
-		flex: 1,
-		justifyContent: 'flex-end',
-		fontFamily: 'Roboto',
-	},
+  imageBg: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    fontFamily: 'Roboto',
+  },
 });
--- src/screens/RegistrationScreen.jsx
+++ src/screens/RegistrationScreen.jsx
@@ -1,74 +1,68 @@
 import { useState } from 'react';
-import {
-	KeyboardAvoidingView,
-	Platform,
-	StyleSheet,
-	Text,
-	View,
-} from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Title, CustomButton, RegistrationForm } from '../components';
 
 export const RegistrationScreen = () => {
-	const [userPhoto, setUserPhoto] = useState(null);
-	return (
-		<SafeAreaView style={styles.container}>
-			<KeyboardAvoidingView
-				behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-				keyboardVerticalOffset={0}
-				style={styles.wrapKeyboard}
-			>
-				<View style={styles.imgWrap}>
-					<CustomButton
-						iconName='pluscircleo'
-						onPress={() => {}}
-						iconSize={25}
-						styleBtn={styles.iconBtn}
-						iconStyle={userPhoto ?? styles.iconActive}
-					/>
-				</View>
-				<Title text='Реєстрація' />
-				<RegistrationForm />
-				<Text style={styles.informText}>Вже є акаунт? Увійти</Text>
-			</KeyboardAvoidingView>
-		</SafeAreaView>
-	);
+  const [userPhoto, setUserPhoto] = useState(null);
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={0}
+        style={styles.wrapKeyboard}
+      >
+        <View style={styles.imgWrap}>
+          <CustomButton
+            iconName="pluscircleo"
+            onPress={() => {}}
+            iconSize={25}
+            styleBtn={styles.iconBtn}
+            iconStyle={userPhoto ?? styles.iconActive}
+          />
+        </View>
+        <Title text="Реєстрація" />
+        <RegistrationForm />
+        <Text style={styles.informText}>Вже є акаунт? Увійти</Text>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 0.66,
-		alignItems: 'center',
-		backgroundColor: '#fff',
-		borderTopLeftRadius: 25,
-		borderTopRightRadius: 25,
-	},
-	wrapKeyboard: {
-		flex: 1,
-		alignItems: 'center',
-		width: '100%',
-		paddingTop: 60,
-	},
-	imgWrap: {
-		width: 120,
-		height: 120,
-		position: 'absolute',
-		top: -60,
+  container: {
+    flex: 0.68,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  wrapKeyboard: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+    paddingTop: 60,
+  },
+  imgWrap: {
+    width: 120,
+    height: 120,
+    position: 'absolute',
+    top: -60,
 
-		backgroundColor: '#f6f6f6',
-		borderRadius: 16,
-	},
-	iconBtn: {
-		right: '-100%',
-		top: '100%',
-		transform: [{ translateY: -37.5 }, { translateX: -12.5 }],
-	},
-	iconActive: {
-		color: '#FF6C00',
-	},
-	informText: {
-		marginTop: 16,
-		fontWeight: 400,
-		color: '#1B4371',
-	},
+    backgroundColor: '#f6f6f6',
+    borderRadius: 16,
+  },
+  iconBtn: {
+    right: '-100%',
+    top: '100%',
+    transform: [{ translateY: -37.5 }, { translateX: -12.5 }],
+  },
+  iconActive: {
+    color: '#FF6C00',
+  },
+  informText: {
+    marginTop: 16,
+    fontWeight: 400,
+    color: '#1B4371',
+  },
 });
