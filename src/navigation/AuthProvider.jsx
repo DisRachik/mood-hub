8db39ade05a62246b2a2d9a1e65dc34a808fb4ad
DisRachik <radchenkoig@gmail.com>
2023-10-16 17:11:52 +0300

    feat: add btn delete in Create post screen

--- src/navigation/AuthProvider.jsx
+++ src/navigation/AuthProvider.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from 'react';
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(true);
 
   const onAccess = () => {
     setIsSignedIn((prevState) => !prevState);
