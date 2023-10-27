e4f043b28dae1ea7b86b2624e6febb9d26b7b8e8
DisRachik <radchenkoig@gmail.com>
2023-10-27 13:32:38 +0300

    feat: connect Redux + Firebase into project

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  name: null,
+  email: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+});
